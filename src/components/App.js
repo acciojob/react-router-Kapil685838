6bd48c4b62dc43c6337be66d0487e11491946137
Kapil Chaudhary <kc685838@gmail.com>
2023-07-01 11:14:45 +0530

    submitting code-Sat Jul 01 2023 11:14:43 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
-
 import React from "react";
 import './../styles/App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "../Pages/Home";
+import About from "../Pages/About";
+import Header from "./Header";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
+      {/* Do not remove the main div */}
     </div>
   )
 }
 
-export default App
+export default App;
